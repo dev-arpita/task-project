c6bbdfab1024f7158355f52df5eea38316fc7dfc
Arpita Roy <arpita.roy.workar@gmail.com>
2023-08-16 16:10:42 +0530

    wip- toggle menu

--- index.js
+++ index.js
@@ -1,26 +1,11 @@
-function toggleMenu() {
-                var menuBox = document.querySelector('.sidebar');
-                if (menuBox.style.display == "block") { // if is menuBox displayed, hide it
-                    // menuBox.style.display = "none";
-                    menuBox.classList.remove("show");
-                    menuBox.classList.add("hide");
-                }
-                else { // if is menuBox hidden, display it
-                    // menuBox.style.display = "block";
-                    menuBox.classList.add("show");
-                }
-            }
-            function toggleFromDesktop() {
-                    var menuBox = document.querySelector('.nav__menu');
-                    console.log(menuBox.classList.contains("show"));
-                    if (menuBox.classList.contains("show")) { // if is menuBox displayed, hide it
-                        // menuBox.style.display = "none";
-                        menuBox.classList.remove("show");
-                        menuBox.classList.add("hide");
-                    }
-                    else { // if is menuBox hidden, display it
-                        // menuBox.style.display = "block";
-                        menuBox.classList.remove("hide");
-                         menuBox.classList.add("show");
-                    }
-                }
+const menuBtn = document.querySelector('.menu-btn');    
+const sideBar = document.querySelector('.sidebar');
+const exitBtn = document.querySelector('.sidebar__btn-menu');
+
+menuBtn.addEventListener('click', function() {
+    sideBar.style.transform = 'translateX(0)';
+})
+
+exitBtn.addEventListener('click', function () {
+        sideBar.style.transform = 'translateX(-100%)';
+    })
