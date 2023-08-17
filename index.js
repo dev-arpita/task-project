fa5039d1d7d65000a8c71820d5a24c08a95e36d3
Arpita Roy <arpita.roy.workar@gmail.com>
2023-08-17 14:07:49 +0530

    wip: optimize fixes

--- index.js
+++ index.js
@@ -2,10 +2,29 @@ const menuBtn = document.querySelector('.menu-btn');
 const sideBar = document.querySelector('.sidebar');
 const exitBtn = document.querySelector('.sidebar__btn-menu');
 
-menuBtn.addEventListener('click', function() {
-    sideBar.style.transform = 'translateX(0)';
-})
+let screen = window.matchMedia("(max-width: 767px)")
+menuBtn.addEventListener('click', ()=> {
+    screen.addListener(myFunctionMenu)
+    myFunctionMenu(screen)
+    }) 
 
-exitBtn.addEventListener('click', function () {
-        sideBar.style.transform = 'translateX(-100%)';
-    })
+exitBtn.addEventListener('click', ()=>{
+    screen.addListener(myFunctionExitMenu)
+    myFunctionExitMenu(screen)
+} )
+
+function myFunctionMenu(x) {
+  if (x.matches) { // If media query matches
+    sideBar.style.transform = 'translateX(0)';
+  } else {
+    sideBar.style.transform = 'none';
+  }
+}
+
+function myFunctionExitMenu(x) {
+  if (x.matches) { // If media query matches
+     sideBar.style.transform = 'translateX(-100%)';
+  } else {
+    sideBar.style.transform = 'none';
+  }
+}
